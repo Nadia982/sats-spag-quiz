66be59894a776a9223da9303038e3f520b5cd3d3
Natalie Gillam <ngillam@hotmail.co.uk>
2023-12-26 23:04:16 +0000

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,203 @@
+const questions = [
+    {
+      id: 1,
+      q: "Which sentence is a command?",
+      options: ["The relay race will be next.", "I hope I don’t drop the baton.", "Run as fast as you can", "I know you can win this race"],
+      answer: 2,
+      questionRef: "2023-paper1-q1"
+    },
+    {
+      id: 2,
+      q: "Is this sentence an exclamation or a question? 'How disappointing it was that it rained on sports day'",
+      options: ["Exclamation", "Question"],
+      answer: 0,
+      questionRef: "2023-paper1-q2a"
+  
+    },
+  
+    {
+      id: 3,
+      q: "Is this sentence an exclamation or a question? 'How many times have you been to the swimming pool this week'",
+      options: ["Exclamation", "Question"],
+      answer: 1,
+      questionRef: "2023-paper1-q2b"
+    },
+  
+    {
+      id: 4,
+      q: "Is this sentence an exclamation or a question? 'How will you spend your summer holidays'",
+      options: ["Exclamation", "Question"],
+      answer: 1,
+      questionRef: "2023-paper1-q2c"
+    },
+  
+    {
+      id: 5,
+      q: "Is this sentence an exclamation or a question? 'How kind you are to give me this present'",
+      options: ["Exclamation", "Question"],
+      answer: 0,
+      questionRef: "2023-paper1-q2d"
+    },
+  
+    {
+      id: 6,
+      q: "Which sentence is written in standard English?",
+      options: ["The autumn leaves was crunching beneath our feet as we walked along.", "The autumn leaves were crunching beneath our feet as we walked along."],
+      answer: 1,
+      questionRef: "2023-paper1-q5a"
+    },
+  
+    {
+      id: 7,
+      q: "Which sentence is written in standard English?",
+      options: ["We done a great job in picking the juiciest fruit for our pie.", "We did a great job in picking the juiciest fruit for our pie."],
+      answer: 1,
+      questionRef: "2023-paper1-q5b"
+    },
+    
+    {
+      id: 8,
+      q: "Which sentence is written in standard English?",
+      options: ["My brother didn't do anything to help us.", "My brother didn't do nothing to help us."],
+      answer: 0,
+      questionRef: "2023-paper1-q5b"
+    },
+    {
+      id: 9,
+      q: "Which pair of words are antonyms?",
+      options: ["marine submarine", "unicycle bicycle", "proper improper", "act interact"],
+      answer: 2,
+      questionRef: "2023-paper1-q6"
+    },
+    {
+      id: 10,
+      q: "Select the sentence that shows Lara is most likely to go swimming on Saturday.",
+      options: ["Lara will go swimming on Saturday.", "Lara should go swimming on Saturday.", "Lara might go swimming on Saturday.", "Lara could go swimming on Saturday."],
+      answer: 1,
+      questionRef: "2023-paper1-q9"
+    },
+    {
+      id: 11,
+      q: "Which sentence is the most formal?",
+      options: ["We can't keep your things here, as there's no room.", "It is essential that you take all your belongings with you.", "Please pack up all your stuff before you leave.", "It’s time to get all your bits and pieces together."],
+      answer: 1,
+      questionRef: "2023-paper1-q9" 
+    },
+    {
+      id: 12,
+      q: "Which sentence is punctuated correctly?",
+      options: ["Charlie's party, which was at the ice rink was really enjoyable.", "Charlie's party which was at the ice rink, was really enjoyable.", "Charlie's party, which was at the ice rink, was really enjoyable.", "Charlie's party which, was at the ice rink, was really enjoyable."],
+      answer: 2,
+      questionRef: "2023-paper1-q11" 
+    },
+    {
+      id: 13,
+      q: "In which sentence is plant a verb?",
+      options: ["The recycling plant was closed for the weekend.",
+      "The children plant beans in the school garden.",
+      "I remember to water my plant every day.",
+      "The roses were growing in a plant pot."],
+      answer: 1,
+      questionRef: "2023-paper1-q12" 
+    },
+    {
+      id: 14,
+      q: "Which sentence is the most formal?",
+      options: ["I asked him to phone me when he got here.",
+      "I told him to call me as soon as he turned up.",
+      "I got him to ring me when he came in.",
+      "I requested that he telephone me on arrival."],
+      answer: 3,
+      questionRef: "2023-paper1-q14"
+    },
+    {
+      id: 15,
+      q: "Is the apostrophe in this sentence used for a contracted form or possession? 'Let's hope it warms up soon.'",
+      options: ["Contracted form", "Possession"],
+      answer: 0,
+      questionRef: "2023-paper1-q17a" 
+    },
+    {
+      id: 16,
+      q: "Is the apostrophe in this sentence used for a contracted form or possession? 'The teachers' coats were hanging in the staffroom.'",
+      options: ["Contracted form", "Possession"],
+      answer: 1,
+      questionRef: "2023-paper1-q17b"
+    },
+    {
+      id: 17,
+      q: "Is the apostrophe in this sentence used for a contracted form or possession? 'The children wear their scarves when it's cold.'",
+      options: ["Contracted form", "Possession"],
+      answer: 0,
+      questionRef: "2023-paper1-q17c"
+    },
+    {
+      id: 18,
+      q: "Which option completes the sentence using the present progressive? 'I _________ the photos to the school website.'",
+      options: ["upload", "will upload", "have uploaded", "am uploading"],
+      answer: 3,
+      questionRef: "2023-paper1-q18" 
+    },
+    {
+      id: 19,
+      q: "Which sentence uses tense correctly?",
+      options: ["She is getting the ball and then she scored the winning goal.",
+      "My friends thought the film was good, but I don't like thrillers.",
+      "When the clock strikes midnight, everyone was in bed.",
+      "Tonight he is playing rounders and she joined in."],
+      answer: 1,
+      questionRef: "2023-paper1-q19"
+    },
+    {
+      id: 20,
+      q: "Which sentence is punctuated correctly?",
+      options: ["Erin wanted the most up-to date computer that was available.", "Erin wanted the most-up-to-date computer that was available.", "Erin wanted the most up-to-date-computer that was available.", "Erin wanted the most up-to-date computer that was available."],
+      answer: 3,
+      questionRef: "2023-paper1-q28" 
+    },
+    {
+      id: 21,
+      q: "What are the words between asterisks in the sentence below? *The girl with curly red hair* is in my class.",
+      options: ["a relative clause", "a subordinate clause", "a main clause", "a noun phrase"],
+      answer: 3,
+      questionRef: "2023-paper1-q33" 
+    },
+    {
+      id: 22,
+      q: "Which sentence uses a semi-colon correctly?",
+      options: ["In the bag we found; five carrots, two cabbages and a large onion.", "The book began with a boy called Tim; climbing a steep hill.", "Jessie went to look for her brother; Bill.", "The wind was howling; the rain was drumming on the roof."],
+      answer: 3,
+      questionRef: "2023-paper1-q38"
+    },
+    {
+      id: 23,
+      q: "Is 'once' an adverb or a conjunction in this sentence? 'I've only been to France once with my family.'",
+      options: ["adverb", "conjunction"],
+      answer: 0,
+      questionRef: "2023-paper1-q50a"
+    },
+    {
+      id: 24,
+      q: "Is 'once' an adverb or a conjunction in this sentence? 'Once I arrived in Paris, I tried to speak French.'",
+      options: ["adverb", "conjunction"],
+      answer: 1,
+      questionRef: "2023-paper1-q50b"
+    },
+    {
+      id: 25,
+      q: "Is 'once' an adverb or a conjunction in this sentence? 'My accent really improved once we had been there a few days.'",  
+      options: ["adverb", "conjunction"],
+      answer: 1,
+      questionRef: "2023-paper1-q50c"
+    },
+    
+  //example of an image-based question
+  // {
+  //   id: 11,
+  //   q: "How many squares are showing in this pic?",
+  //   options: ["3", "2", "1"],
+  //   answer: 2,
+  //   img: "img/square.jpg"
+  // questionRef:
+  //   }  
+  ];
---
+++ js/script.js
@@ -0,0 +1,254 @@
+const questionNumber = document.querySelector(".question-number");
+
+const questionText = document.querySelector(".question-text");
+
+const optionContainer = document.querySelector(".option-container");
+const answersIndicatorContainer = document.querySelector(".answers-indicator");
+const homeBox = document.querySelector(".home-box");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const nextButton = document.querySelector(".next-btn");
+const questionLimit = 5;
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+let correctAnswers = 0;
+let attempt = 0;
+
+// add the questions to the availableQuestions array
+function setAvailableQuestions() {
+  const totalQuestions = questions.length;
+  for (let i = 0; i < totalQuestions; i++) {
+    availableQuestions.push(questions[i]);
+  }
+}
+
+//set question number, question text and answer options
+function getNewQuestion() {
+  nextButton.classList.add("hide");
+
+  //set question number
+  questionNumber.innerHTML = `Question ${
+    questionCounter + 1
+  } of ${questionLimit}`;
+  //set question text
+  //get random question
+  const questionIndex =
+    availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+  currentQuestion = questionIndex;
+  questionText.innerHTML = currentQuestion.q;
+
+  // get the position of "QuestionIndex" from the "AvailableQuestions" array
+
+  const index1 = availableQuestions.indexOf(questionIndex);
+
+  //remove questionIndex from the ‘AvailableQuestions’ array
+
+  availableQuestions.splice(index1, 1);
+
+  // show question image if "img" property exists
+
+  if (currentQuestion.hasOwnProperty("img")) {
+    console.log(currentQuestion.img);
+    const img = document.createElement("img");
+    img.src = currentQuestion.img;
+    questionText.appendChild(img);
+  }
+
+  // set options
+  // get the length of the list of options
+  const optionsLength = currentQuestion.options.length;
+
+  optionContainer.innerHTML = "";
+
+  // push options into availableOptions array
+
+  for (let i = 0; i < optionsLength; i++) {
+    availableOptions.push(i);
+  }
+
+  //create options in html
+  let animationDelay = 0.1;
+
+  for (let i = 0; i < optionsLength; i++) {
+    const optionIndex =
+      availableOptions[Math.floor(Math.random() * availableOptions.length)];
+
+    //get the position of optionIndex from availableOptions
+
+    const index2 = availableOptions.indexOf(optionIndex);
+
+    //remove the “optionIndex” from the availableOptions so that the option does not repeat
+
+    availableOptions.splice(index2, 1);
+
+    const option = document.createElement("button");
+    option.innerHTML = currentQuestion.options[optionIndex];
+    option.id = optionIndex;
+    option.style.animationDelay = animationDelay + "s";
+    option.tabIndex = i + 1;
+    console.log(option.tabIndex);
+
+    animationDelay = animationDelay + 0.1;
+
+    option.className = "option";
+    optionContainer.appendChild(option);
+    option.setAttribute("onclick", "getResult(this)");
+
+    option.addEventListener("keydown", pressEnterToGetResult);
+
+    function pressEnterToGetResult(e) {
+      if (e.key == "Enter") {
+        option.removeEventListener("keydown", pressEnterToGetResult);
+        getResult(this);
+        unclickableOptions();
+      }
+    }
+  }
+  questionCounter++;
+}
+
+function getResult(element) {
+  unclickableOptions();
+  const id = parseInt(element.id);
+  //get the answer by comparing the id of the clicked option
+  if (id === currentQuestion.answer) {
+    // add green colour if user selects correct option
+    element.classList.add("correct");
+    //add a tick mark to the answer indicator
+    updateAnswerIndicator("correct");
+    correctAnswers++;
+
+    // availableOptions.removeEventListener("keydown", pressEnterToGetResult);
+  } else {
+    // add red colour if user selects incorrect option
+    element.classList.add("incorrect");
+    //add a cross mark to the answer indicator
+    updateAnswerIndicator("incorrect");
+
+    //if answer is incorrect then show the correct answer
+    const optionsLength = optionContainer.children.length;
+    for (let i = 0; i < optionsLength; i++) {
+      setTimeout(() => {
+        if (
+          parseInt(optionContainer.children[i].id) === currentQuestion.answer
+        ) {
+          optionContainer.children[i].classList.add("correct");
+        }
+      }, 400);
+    }
+  }
+  attempt++;
+  // document.addEventListener("keydown", pressEnterForNextQu);
+  // next();
+  nextButton.classList.remove("hide");
+}
+
+//add shortcut key for the return key to go to the next question
+function pressEnterForNextQu(e) {
+  if (e.key === "Enter") {
+    next();
+  }
+}
+
+//make other options unclickable once user has selected an option
+function unclickableOptions() {
+  const optionsLength = optionContainer.children.length;
+  for (let i = 0; i < optionsLength; i++) {
+    optionContainer.children[i].classList.add("already-answered");
+    optionContainer.children[i].setAttribute("disabled", "");
+  }
+}
+
+//creating answersIndicator box, and answer indicator circles for each question
+function answersIndicator() {
+  answersIndicatorContainer.innerHTML = "";
+  const totalQuestion = questionLimit;
+  for (let i = 0; i < totalQuestion; i++) {
+    const indicator = document.createElement("div");
+    answersIndicatorContainer.appendChild(indicator);
+  }
+}
+
+function updateAnswerIndicator(markType) {
+  answersIndicatorContainer.children[questionCounter - 1].classList.add(
+    markType
+  );
+}
+
+function next() {
+  // document.removeEventListener("keydown", pressEnterForNextQu);
+  if (questionCounter >= questionLimit) {
+    quizOver();
+  } else {
+      getNewQuestion();
+  }
+}
+
+function quizOver() {
+  //hide quizBox
+  quizBox.classList.add("hide");
+  //show resultBox
+  resultBox.classList.remove("hide");
+  quizResult();
+}
+
+//get the quiz result
+function quizResult() {
+  resultBox.querySelector(".total-question").innerHTML = questionLimit;
+  resultBox.querySelector(".total-attempts").innerHTML = attempt;
+  resultBox.querySelector(".correct-answers").innerHTML = correctAnswers;
+  resultBox.querySelector(".wrong-answers").innerHTML =
+    attempt - correctAnswers;
+  const percentage = (correctAnswers / questionLimit) * 100;
+  resultBox.querySelector(".percent-correct").innerHTML =
+    percentage.toFixed(2) + "%";
+  resultBox.querySelector(".total-score").innerHTML =
+    correctAnswers + "/" + questionLimit;
+}
+
+function resetQuiz() {
+  questionCounter = 0;
+  correctAnswers = 0;
+  attempt = 0;
+  availableQuestions = [];
+}
+
+function tryAgainQuiz() {
+  //hide the result box
+  resultBox.classList.add("hide");
+
+  //show the quiz box
+  quizBox.classList.remove("hide");
+
+  resetQuiz();
+  startQuiz();
+}
+
+function goToHome() {
+  //hide result box
+  resultBox.classList.add("hide");
+  // show home box
+  homeBox.classList.remove("hide");
+  resetQuiz();
+}
+
+function startQuiz() {
+  // hide home box
+  homeBox.classList.add("hide");
+  //hide result box
+  resultBox.classList.add("hide");
+  // show quiz box
+  quizBox.classList.remove("hide");
+
+  setAvailableQuestions();
+  getNewQuestion();
+  // to create indicator of answers
+  answersIndicator();
+}
+
+window.onload = function () {
+  homeBox.querySelector(".total-questions").innerHTML = questionLimit;
+};
